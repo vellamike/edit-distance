b782752eea323aa15223985d58018269304b449c
Mike Vella <vellamike@gmail.com>
2016-01-16 02:51:38 +0000

    Implemented the recursive option

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+import "math"
+
+func recursive (string1, string2 string) float64{
+//	fmt.Println(string1)
+//	fmt.Println(string2)
+	if (len(string1) == 0) {
+		return float64(len(string2))
+	}
+
+	if (len(string2) == 0) {
+		return float64(len(string1))
+	}
+
+	if (len(string2) == 0) &&  (len(string1) == 0) {
+		return 0.0
+	}
+	
+	mismatch_1 := recursive(string1, string2[0:len(string2) -1]) + 1.0
+	mismatch_2 := recursive(string1[0:len(string1) -1], string2 ) + 1.0
+
+	var cost float64
+	if string1[len(string1)-1] == string2[len(string2)-1] {
+		cost = 0.0
+	} else {
+		cost = 1.0
+	}
+	mismatch_3 := recursive(string1[0:len(string1) -1], string2[0:len(string2) -1]) + cost
+
+	return math.Min(math.Min(mismatch_1, mismatch_2), mismatch_3)
+}
+
+func main(){
+
+	str1 := "abcdefghijklm"
+	str2 := "abcdefghijklm"
+	fmt.Println(recursive(str1, str2))
+}
